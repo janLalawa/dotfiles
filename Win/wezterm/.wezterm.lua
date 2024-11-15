3d065e076498500e3e49e1af921737a511d68a3f
Laura <40493346+janLalawa@users.noreply.github.com>
2024-11-15 10:28:08 +0000

    Wezterm fixes

--- Win/wezterm/.wezterm.lua
+++ Win/wezterm/.wezterm.lua
@@ -1,21 +1,15 @@
-local wezterm = require('wezterm')
+local wezterm = require 'wezterm'
 local config = {}
 
 -- Color scheme
 config.color_scheme = 'Catppuccin Mocha'
 
--- Font configuration
-config.font = wezterm.font({
-    family = 'Hack Nerd Font',
-    harfbuzz_features = {'calt=0', 'clig=0', 'liga=0'} -- Disable ligatures
-})
-config.font_size = 11.0
-config.line_height = 1.0  -- Adjust line height multiplier
-config.cell_width = 1.0   -- Adjust cell width multiplier
+config.line_height = 1.0 
+config.cell_width = 1.0  
 
 -- Window appearance
 config.window_background_opacity = 0.95
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.window_decorations = "TITLE | RESIZE"
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
@@ -25,13 +19,22 @@ config.window_padding = {
     bottom = 8,
 }
 
+-- Font configuration
+config.font = wezterm.font_with_fallback({
+    'Hack Nerd Font',
+    'Symbols Nerd Font Mono',  
+
+})
+config.font_size = 11.0
+config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }  -- Enable ligatures
+
 -- Cursor configuration
 config.default_cursor_style = 'BlinkingBlock'
 config.cursor_blink_rate = 500
 
 -- Key bindings
 config.keys = {
-    -- Add custom key bindings here
+   
 }
 
 -- Disable default updates
@@ -41,8 +44,31 @@ config.check_for_updates = false
 config.enable_scroll_bar = true
 
 -- Font rendering
-config.freetype_load_target = "Light"  -- Adjust font rendering
+config.freetype_load_target = "Light"  
 config.freetype_render_target = "HorizontalLcd"
+
+-- Default shell configuration for Windows
+config.default_prog = {
+    'pwsh.exe',  
+}
+
+-- Enable Windows environment features
+config.term = 'wezterm'
+config.set_environment_variables = {
+    TERM = "wezterm",
+    COLORTERM = "truecolor",
+    TERM_PROGRAM = "WezTerm",
+}
+
+-- Tab bar customization
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+
+-- Better handling of Git and other CLI tools
+config.enable_kitty_keyboard = true
+config.enable_csi_u_key_encoding = true
+
+config.default_domain = 'local'
 
 return config
 
