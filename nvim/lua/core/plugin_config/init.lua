979a93665af3ad5ce622202c175eace833392ba9
Laura <40493346+janLalawa@users.noreply.github.com>
2024-11-20 12:22:04 +0000

    Comment out nvim-tree-config

--- nvim/lua/core/plugin_config/init.lua
+++ nvim/lua/core/plugin_config/init.lua
@@ -1,5 +1,5 @@
 require("core.plugin_config.lualine")
-require("core.plugin_config.nvim-tree-config")
+--require("core.plugin_config.nvim-tree-config")
 require("core.plugin_config.catppuccin-config")
 require("core.plugin_config.mason")
 require("core.plugin_config.lsp_config")
